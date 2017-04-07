375d3b38825c650551032efe906ef72f2c1463d2
Isaac Schemm <isaacschemm@gmail.com>
2017-04-07 18:08:49 -0500

    Change default external browser to Chrome Chrome seems to load the LastPass extension faster than Firefox does.

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -1,3 +1,3 @@
 pref("extensions.vault-shortcut.exe-path", "");
 pref("extensions.vault-shortcut.lastpass-url", "");
-pref("extensions.vault-shortcut.browser", "firefox");
+pref("extensions.vault-shortcut.browser", "chrome");
